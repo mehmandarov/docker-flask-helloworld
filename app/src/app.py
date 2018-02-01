e4c82fcf9b54e2d3ff48cfe20e55bb4be2a0aac9
Rustam Mehmandarov <mehmandarov@gmail.com>
2018-02-02 00:24:43 +0100

    Added another sample resource

--- app/src/app.py
+++ app/src/app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def hello_world():
     return 'Flask: Hello World from Docker'
 
+@app.route('/api')
+def rest_hello_world():
+    return '{"id":1,"message":"Flask: Hello World from Docker"}'
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
